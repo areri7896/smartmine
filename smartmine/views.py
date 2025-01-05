d0372f58a78b49e624e1626ca87f5d3cc39a9ab9
rodney areri <65330917+areri7896@users.noreply.github.com>
2025-01-05 13:12:36 +0300

    updating the decorators

--- smartmine/views.py
+++ smartmine/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_not_required
+
+# import pandas as pd
+# from dotenv import load_dotenv
+# load_dotenv()
+# import os
 
 # Create your views here.
 
+@login_not_required
 def index(request):
+#     from binance.client import Client
+
+# # Test 
+#     api_key = os.environ['BINANCE_API_KEY_TEST']
+#     api_secret = os.environ['BINANCE_API_SECRET_TEST']
+#     client = Client(api_key, api_secret, testnet=True)
+    
     return render(request, 'src/landing/index.html', {})
 
 # def login(request):
